3a77f9c20681101b3253f334d38ebd3d40774926
Quinten De Clerck <quintendc@users.noreply.github.com>
2020-06-14 22:48:04 +0200

    added SaveGameObject

---
+++ Toolkit/Assets/Toolkit/Scripts/SaveGameObject.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MySaveGameObject", menuName = "Toolkit/SaveGameObject", order = 1)]
+public class SaveGameObject : ScriptableObject
+{
+
+}
