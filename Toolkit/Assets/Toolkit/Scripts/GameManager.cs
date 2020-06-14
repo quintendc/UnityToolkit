98cb41057603efca0cf322c3c76942441db671b8
Quinten De Clerck <quintendc@users.noreply.github.com>
2020-06-14 19:45:26 +0200

    Added GameManager script and Prefab

---
+++ Toolkit/Assets/Toolkit/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance = null;
+
+
+    private void Awake()
+    {
+        #region Singleton Pattern GameManager instance
+
+        // check if instance already exists
+        if (Instance == null)
+        {
+            // if not, set instance to this
+            Debug.Log("GameManager Instance created");
+            Instance = this;
+        }
+        // if instance already exists and it's not this:
+        else if (Instance != null)
+        {
+            // then destroy this. this enforces our singleton pattern, meaning there can only ever be one instance of the GameManager
+            Debug.Log("GameManager Instance already exists this.destroyed");
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(this);
+
+        #endregion
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
