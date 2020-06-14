8a82c312a2ec8c49c0be6434aab41bea52cd61f7
Quinten De Clerck <quintendc@users.noreply.github.com>
2020-06-14 21:34:19 +0200

    added SceneLoaded Callback

--- Toolkit/Assets/Toolkit/Scripts/GameManager.cs
+++ Toolkit/Assets/Toolkit/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -36,12 +38,18 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("scene loaded");
+        //do stuff
     }
 }
