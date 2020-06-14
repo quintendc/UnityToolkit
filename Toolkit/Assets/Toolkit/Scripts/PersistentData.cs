a03cb5c9acd38aa3b30902456255a40604c9f348
Quinten De Clerck <quintendc@users.noreply.github.com>
2020-06-14 22:18:13 +0200

    added persistent Data Object

---
+++ Toolkit/Assets/Toolkit/Scripts/PersistentData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PersistentDataObject", menuName = "Toolkit/PeristentDataObject", order = 0)]
+public class PersistentData : ScriptableObject
+{
+    // place variables that your need throughout the entire game session here
+}
