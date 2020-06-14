71e2f4500089c0b65de28317f51715abf64f325a
Quinten De Clerck <quintendc@users.noreply.github.com>
2020-06-14 22:48:41 +0200

    changes PersistentData

--- Toolkit/Assets/Toolkit/Scripts/PersistentData.cs
+++ Toolkit/Assets/Toolkit/Scripts/PersistentData.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "PersistentDataObject", menuName = "Toolkit/PeristentDataObject", order = 0)]
+[CreateAssetMenu(fileName = "MyPersistentDataObject", menuName = "Toolkit/PeristentDataObject", order = 0)]
 public class PersistentData : ScriptableObject
 {
     // place variables that your need throughout the entire game session here
