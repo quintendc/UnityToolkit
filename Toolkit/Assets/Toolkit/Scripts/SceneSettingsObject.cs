2542a81e06108b5ee38025e6226b649c52c941b3
Quinten De Clerck <quintendc@users.noreply.github.com>
2020-06-15 18:41:48 +0200

    added headers to SceneSettingsObject

--- Toolkit/Assets/Toolkit/Scripts/SceneSettingsObject.cs
+++ Toolkit/Assets/Toolkit/Scripts/SceneSettingsObject.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MySceneSettingsObject", menuName = "Toolkit/SceneSettingsObject", order = 2)]
 public class SceneSettingsObject : ScriptableObject
 {
+    [Header("Widget parameters")]
     [Tooltip("define which Widget should be shown when the scene is loaded.")]
     public WidgetTypes WidgetType;
 
     // widgetRenderCamera -> get maincamera when no RenderCamera is provided
     public Camera Camera = null;
 
+    [Header("GameMode parameters")]
     // gameMode to instantiate
     public GameObject GameMode = null;
 
