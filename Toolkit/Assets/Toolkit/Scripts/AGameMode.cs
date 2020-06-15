be6c6947b827809968de0c3bdf61f24122687ffb
Quinten De Clerck <quintendc@users.noreply.github.com>
2020-06-15 22:39:27 +0200

    added Spawnpoints query to AGameMode

--- Toolkit/Assets/Toolkit/Scripts/AGameMode.cs
+++ Toolkit/Assets/Toolkit/Scripts/AGameMode.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public abstract class AGameMode : MonoBehaviour
@@ -20,4 +21,49 @@ public abstract class AGameMode : MonoBehaviour
     {
         
     }
+
+
+    #region Methods
+
+
+    /// <summary>
+    /// get a list of all spawnpoints
+    /// </summary>
+    /// <returns></returns>
+    public List<GameObject> GetAllSpawnpoints()
+    {
+        List<GameObject> spawnpoints = GameObject.FindGameObjectsWithTag("Spawnpoint").ToList();
+
+        return spawnpoints;
+    }
+
+
+    /// <summary>
+    /// get a list of all spawnpoints where PlayerIndex is equal to ...
+    /// </summary>
+    /// <param name="playerIndex">playerIndex filter</param>
+    /// <returns></returns>
+    public List<GameObject> GetSpawnpointsByPlayerIndex(int playerIndex)
+    {
+        List<GameObject> spawnpoints = GetAllSpawnpoints();
+        List<GameObject> filterdSpawnpoints = (List<GameObject>)spawnpoints.Where(s => s.GetComponent<Spawnpoint>().PlayerIndex == playerIndex);
+
+        return filterdSpawnpoints;
+    }
+
+
+    /// <summary>
+    /// get a spawnpoint from all spawnpoints at index ...
+    /// </summary>
+    /// <param name="index">index of the spawnpoint</param>
+    /// <returns></returns>
+    public GameObject GetSpawnpointByIndex(int index)
+    {
+        List<GameObject> spawnpoints = GetAllSpawnpoints();
+
+        return spawnpoints.ElementAt(index);
+    }
+    #endregion
+
+
 }
