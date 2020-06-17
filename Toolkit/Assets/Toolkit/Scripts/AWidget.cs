bc9600426e1e09b70c7b535793b14ace2dc3312b
Quinten De Clerck <quintendc@users.noreply.github.com>
2020-06-17 18:48:29 +0200

    changes AWidget

--- Toolkit/Assets/Toolkit/Scripts/AWidget.cs
+++ Toolkit/Assets/Toolkit/Scripts/AWidget.cs
@@ -1,8 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class AWidget : ToolkitBehaviour
 {
+    public Selectable InitialSelectedElement = null;
     public WidgetTypes WidgetType;
+
+    public void Start()
+    {
+        if (InitialSelectedElement != null)
+        {
+            InitialSelectedElement.Select();
+        }
+        else
+        {
+            Debug.LogWarning("No initial selected element provided some functions may not work");
+        }
+    }
 }
