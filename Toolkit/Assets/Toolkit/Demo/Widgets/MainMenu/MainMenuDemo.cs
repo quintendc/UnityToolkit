43c65f51623e629f627f726c0527d8202b4c86ec
Quinten De Clerck <quintendc@users.noreply.github.com>
2020-06-19 17:25:09 +0200

    added CustomManagers, widgets, SceneSettingsObjects

--- Toolkit/Assets/Toolkit/Demo/Widgets/MainMenu/MainMenuDemo.cs
+++ Toolkit/Assets/Toolkit/Demo/Widgets/MainMenu/MainMenuDemo.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuDemo : AWidget
 {
     public void Play()
     {
         // load scene1
+        SceneManager.LoadScene("Scene1");
     }
 
     public void Settings()
