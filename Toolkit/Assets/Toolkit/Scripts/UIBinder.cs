a6876b4ebe876842a014fab6c1b5d8a9d37b7891
Quinten De Clerck <quintendc@users.noreply.github.com>
2020-06-22 22:37:05 +0200

    got working UI Binder

--- Toolkit/Assets/Toolkit/Scripts/UIBinder.cs
+++ Toolkit/Assets/Toolkit/Scripts/UIBinder.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class UIBinder : MonoBehaviour
@@ -8,41 +9,32 @@ public class UIBinder : MonoBehaviour
     [Tooltip("Set Text")]
     public Text Target = null;
 
-    [Header("Source")]
-    [Tooltip("select the type of source")]
-    public SourceType SourceType;
-
     [Tooltip("read property")]
     public Selectable Source = null;
 
-    private void Update()
-    {
-        //switch (SourceType)
-        //{
-        //    case SourceType.Slider:
-        //        Target.text = (Slider)Source.Value
-        //        break;
-        //    case SourceType.CheckBox:
-        //        break;
-        //    case SourceType.InputField:
-        //        break;
-        //    default:
-        //        break;
-        //}
-    }
 
+    #region Type Methods
 
+    /// <summary>
+    /// method for slider
+    /// </summary>
+    /// <param name="value">value delivered by the Slider</param>
     public void SliderChanged(float value)
     {
         Target.text = value.ToString();
     }
 
+    /// <summary>
+    /// method for InputField
+    /// </summary>
+    /// <param name="text">text delivered by the InputField/param>
+    public void OnTextChanged(string text)
+    {
+        Target.text = text;
+    }
 
-}
+    #endregion
 
-public enum SourceType
-{
-    Slider,
-    CheckBox,
-    InputField
+
+
 }
