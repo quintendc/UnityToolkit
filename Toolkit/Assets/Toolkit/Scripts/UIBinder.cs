6cbba07ebb459fc3a9427eb9a7dc5656ced4f36d
Quinten De Clerck <quintendc@users.noreply.github.com>
2020-06-22 16:40:05 +0200

    working on UI Binder

---
+++ Toolkit/Assets/Toolkit/Scripts/UIBinder.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBinder : MonoBehaviour
+{
+    [Tooltip("Set Text")]
+    public Text Target = null;
+
+    [Header("Source")]
+    [Tooltip("select the type of source")]
+    public SourceType SourceType;
+
+    [Tooltip("read property")]
+    public Selectable Source = null;
+
+    private void Update()
+    {
+        //switch (SourceType)
+        //{
+        //    case SourceType.Slider:
+        //        Target.text = (Slider)Source.Value
+        //        break;
+        //    case SourceType.CheckBox:
+        //        break;
+        //    case SourceType.InputField:
+        //        break;
+        //    default:
+        //        break;
+        //}
+    }
+
+
+    public void SliderChanged(float value)
+    {
+        Target.text = value.ToString();
+    }
+
+
+}
+
+public enum SourceType
+{
+    Slider,
+    CheckBox,
+    InputField
+}
