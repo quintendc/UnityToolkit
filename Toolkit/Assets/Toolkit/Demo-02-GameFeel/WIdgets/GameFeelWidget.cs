e266664d75668570b8c9adcded6af844a7ec8087
Quinten De Clerck <quintendc@users.noreply.github.com>
2020-06-27 16:42:43 +0200

    added Demo Scene, camerashake class

---
+++ Toolkit/Assets/Toolkit/Demo-02-GameFeel/WIdgets/GameFeelWidget.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFeelWidget : AWidget
+{
+
+    public CameraShake cameraShake;
+
+    public void CameraShake()
+    {
+        StartCoroutine(cameraShake.Shake(15f, .4f));
+    }
+}
---
+++ Toolkit/Assets/Toolkit/Scripts/CameraShake.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class CameraShake : ToolkitBehaviour
+{
+
+    #region public properties
+
+    [Header("Shake axis")]
+    public bool ShakeX = true;
+    public bool ShakeY = true;
+    public bool ShakeZ = false;
+
+    #endregion
+
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 originalPos = Camera.main.transform.localPosition;
+
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+            float z = Random.Range(-1f, 1f) * magnitude;
+
+            Camera.main.transform.localPosition =  new Vector3((ShakeX == true) ? x : originalPos.x,
+                                                               (ShakeY == true) ? y : originalPos.y,
+                                                               (ShakeZ == true) ? z : originalPos.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        Camera.main.transform.localPosition = originalPos;
+
+    }
+
+
+}
