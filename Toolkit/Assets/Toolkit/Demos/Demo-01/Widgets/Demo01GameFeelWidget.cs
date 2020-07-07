4ef80e93aab0f3322f97ab26530edb5f93937f0a
Quinten De Clerck <quinten.de.clerck@outlook.com>
2020-07-07 09:48:09 +0200

    working on Demo 01
    - main menu
    - game feel menu and scene

---
+++ Toolkit/Assets/Toolkit/Demos/Demo-01/Widgets/Demo01GameFeelWidget.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo01GameFeelWidget : AWidget
+{
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public void CameraShake()
+    {
+        ToolkitCameraShake();
+    }
+
+    public void FreezeGame()
+    {
+        MotionFX(0f, 0.15f);
+    }
+
+    public void SpeedGame()
+    {
+        MotionFX(2f, 0.15f);
+    }
+
+}
---
+++ Toolkit/Assets/Toolkit/Demos/Demo-01/Widgets/Demo01MainMenuWidget.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Demo01MainMenuWidget : AWidget
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void LoadGameFeelScene()
+    {
+        SceneManager.LoadScene("GameFeelScene");
+    }
+
+    public void ShowSettings()
+    {
+
+    }
+}
