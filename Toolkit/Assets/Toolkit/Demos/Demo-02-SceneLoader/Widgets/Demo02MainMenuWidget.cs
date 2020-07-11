e06f2d019ee024d525166f92e54ec10877369ebb
Quinten De Clerck <quinten.de.clerck@outlook.com>
2020-07-11 20:05:07 +0200

    changes made to Demo02 Main Menu widget

--- Toolkit/Assets/Toolkit/Demos/Demo-02-SceneLoader/Widgets/Demo02MainMenuWidget.cs
+++ Toolkit/Assets/Toolkit/Demos/Demo-02-SceneLoader/Widgets/Demo02MainMenuWidget.cs
@@ -5,14 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Demo02MainMenuWidget : AWidget
 {
-    public void LoadScene01Additive()
+    public void LoadScene01Async()
     {
+        // scene 01 is a child of the PersistentScene
         GetToolkitSceneLoader().LoadSceneAsync("Scene01", LoadSceneMode.Additive);
     }
 
-    public void LoadScene02Single()
+    public void LoadScene02()
     {
-        //GetToolkitSceneLoader().LoadSceneAsync("Scene02", LoadSceneMode.Additive);
+        // load/switch to Scene 02
         GetToolkitSceneLoader().LoadScene("Scene02");
     }
 }
