04cefcf15e93bf60fb3f86afa5f0248d6ede79b2
Quinten De Clerck <quinten.de.clerck@outlook.com>
2020-07-11 17:02:46 +0200

    moved LoadingScreen sample widget, added main menu for demo 02

---
+++ Toolkit/Assets/Toolkit/Demos/Demo-02-SceneLoader/Widgets/Demo02MainMenuWidget.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo02MainMenuWidget : AWidget
+{
+    public void LoadScene01()
+    {
+
+    }
+}
