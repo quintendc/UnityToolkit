ee39e93a3ba39d94ef44e23893c02976e4b51845
Quinten De Clerck <quinten.de.clerck@outlook.com>
2020-07-14 17:17:51 +0200

    Added ToolkitData Class

---
+++ Toolkit/Assets/Toolkit/Scripts/ToolkitData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// DONT'T MAKE CHANGES TO THIS CLASS, these variables are required for the toolkit to make some functionality work!
+/// </summary>
+public static class ToolkitData
+{
+
+    public static List<WidgetTypes> WidgetStack = new List<WidgetTypes>();
+
+}
